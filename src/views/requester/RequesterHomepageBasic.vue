553deb842b16c415d7e6e6f0cf97d4a8188edd50
HumbabaZ <972230255@qq.com>
2018-12-11 20:03:43 +0800

    Update RequesterHomepageBasic.vue

--- src/views/requester/RequesterHomepageBasic.vue
+++ src/views/requester/RequesterHomepageBasic.vue
@@ -2,41 +2,44 @@
 <div>
     <RequesterHomepageTopbar/>
     <el-row>
-        <el-col :span="4">          
+        <el-col :span="4">
             <RequesterHomepageSidebar/>
         </el-col>
-        <el-col :span="20" style="background-color:#F7F6F6">         
+        <el-col :span="20" style="background-color:#F7F6F6">
                 <div class="basic_info">
                     <div class="basic_info_title">
                         <span>基本信息</span>
                     </div>
                     <div class="box">
-                        <el-form label-position="left" label-width="110px" :model="user" >                                            
+                        <el-form label-position="left" label-width="110px">
                         <el-form-item label="用户名">
-                        <el-input v-model="username"></el-input>
+                          <el-input v-model="requester.username"></el-input>
                         </el-form-item>
-                        <el-form-item label="密码">
-                            <el-input type="password" v-model="pwd"></el-input>
+                          <el-form-item label="真实姓名">
+                            <el-input v-model="requester.name"></el-input>
+                          </el-form-item>
+                          <el-form-item label="性别">
+                          <el-input v-model="requester.gender"></el-input>
                         </el-form-item>
+                          <el-form-item label="年龄">
+                            <el-input v-model="requester.age"></el-input>
+                          </el-form-item>
                         <el-form-item label="邮箱">
-                        <el-input v-model="email"></el-input>
+                        <el-input v-model="requester.eMail"></el-input>
                         </el-form-item>
                         <el-form-item label="手机号码">
-                        <el-input v-model="tele"></el-input>
+                        <el-input v-model="requester.teleNumber"></el-input>
                         </el-form-item>
                         <el-form-item>
                             <el-button  type="primary" @click="modify" class="login_button">保存信息</el-button>
                         </el-form-item>
-                        
-                        
-                    </el-form-item>
-                </el-form>
+                      </el-form>
                     </div>
                 </div>
-            
+
         </el-col>
     </el-row>
-       
+
 </div>
 </template>
 
@@ -44,69 +47,49 @@
 <script>
 import RequesterHomepageTopbar from '@/components/RequesterNavi/RequesterHomepageTopbar.vue'
 import RequesterHomepageSidebar from '@/components/RequesterNavi/RequesterHomepageSidebar.vue'
-import axios from 'axios'   
+import * as axios from 'axios'
 
-   export default {
+    export default {
         components:{
             RequesterHomepageTopbar,
             RequesterHomepageSidebar
         },
-        created:function(){
-            this.getBasics();
-        },
         methods: {
-            getBasics(){
-                axios(
-                    {
-                        url:'http://localhost:8080/requester/find-by-username',
-                        method:'POST',
-                        params:{
-                            username:this.$route.query.name
-                        }
-                    }
-                ).then((response)=>{
-                    if (response.data.status == 200) {
-                    }
-                    else
-                        throw response;
-                    if (response.data.status == "200") {
-                        this.username=response.username;
-                        this.email=response.email;
-                        this.tele=response.teleNumber;
-                        this.pwd=response.password;
-                    }
-                }).catch(function (error) {
-                    if (error.status == 500) {
-                    swal("Error", "服务器错误！", "error");
-                    }
-                });     
-            },
-            modify(){
-                axios.post('http://localhost:8080/requester/find-by-username',{
-                    "username":this.username,
-                    "email":this.email,
-                    "tele":this.teleNumber,
-                    "pwd":this.password
-                }).then(function(response){
-                    if(response.status == 200) {
-                    this.$message({
-                    message: '修改成功',
-                    type: 'success'
-                    });
-                    }
-                }).catch(function(error){
-                   
-                    });
-            }
+
         },
         data(){
-           return{
-                username:username,
-                email:email,
-                tele:tele,
-                pwd:pwd
-           }
+            return{
+               requester:{
+                 requesterId: 0,
+                 username: "",
+                 name: "",
+                 teleNumber: "",
+                 eMail: "",
+                 research_field:"",
+                 institutionName:'',
+                 address: "",
+                 payMethod: "",
+                 gender:"",
+                 age: 30,
+                 balance:0,
+                 email:'',
+               }
+            }
         },
+        created(){
+          let that = this;
+          axios({
+            method:	'get',
+            url: '/api/requester/find-myself',
+          })
+            .then(function (response) {
+              console.log(response);
+             that.requester = response.data.requester;
+            })
+            .catch(function (error) {
+              alert(error);
+            });
+        }
     }
 </script>
 
