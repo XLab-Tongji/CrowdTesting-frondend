3a77d7e0eadcb9456f04216dcd558fff1a5d0982
HumbabaZ <972230255@qq.com>
2018-12-11 20:09:14 +0800

    Update WorkerInformation.vue

--- src/views/worker/WorkerInformation.vue
+++ src/views/worker/WorkerInformation.vue
@@ -2,7 +2,8 @@
   <el-container>
     <!--导航-->
     <WorkerHomepageTopbar/>
-    <el-header height="30px" style=""></el-header>
+    <el-header height="30px" style="">
+    </el-header>
 
     <el-main v-if="page==4">
       <el-row>
@@ -35,34 +36,32 @@
             </el-submenu>
           </el-menu>
         </el-col>
-        <el-col span="1">
+        <el-col :span="1">
           <div style="color:#ffffff;width:100%;height:8vh"></div>
         </el-col>
-        <el-col span="17">
-          <el-collapse accordion v-if="information_page_menu==0.0">
+        <el-col :span="16">
+          <el-collapse accordion v-if="information_page_menu==0.0" v-model="activeName">
+            <el-collapse-item name="1">
               <template>
                 <el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
 
                   <el-form-item label="真实姓名">
-                    <el-input v-model="worker.username"></el-input>
+                    <el-input v-model="worker.name"></el-input>
                   </el-form-item>
                   <el-form-item label="性别">
-                    <el-select v-model="form.gender" placeholder="">
-                      <el-option label="女" value="女"></el-option>
-                      <el-option label="男" value="男"></el-option>
+                    <el-select v-model="worker.gender" placeholder="">
+                      <el-option label="女" value="0"></el-option>
+                      <el-option label="男" value="1"></el-option>
                     </el-select>
                   </el-form-item>
                   <el-form-item label="昵称">
-                    <el-input v-model="worker.name"></el-input>
+                    <el-input v-model="worker.username"></el-input>
                   </el-form-item>
                   <el-form-item label="手机号">
                     <el-input v-model="worker.teleNumber"></el-input>
                   </el-form-item>
                   <el-form-item label="邮箱">
                     <el-input v-model="worker.email"></el-input>
-                  </el-form-item>
-                  <el-form-item label="密码">
-                    <el-input v-model="form.password"></el-input>
                   </el-form-item>
                   <el-form-item>
                     <el-button type="primary">保存</el-button>
@@ -72,57 +71,22 @@
               </template>
               <div>
               </div>
-        
+            </el-collapse-item>
           </el-collapse>
           <el-collapse accordion v-if="information_page_menu==1.1">
             <template>
-              <el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
-
-                <el-form-item label="教育背景">
-                  <el-select v-model="worker.education" placeholder="">
-                    <el-option label="本科" value="本科"></el-option>
-                    <el-option label="研究生" value="研究生"></el-option>
-                    <el-option label="博士生" value="博士生"></el-option>
-                  </el-select>
-                </el-form-item>
-
+              <el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;margin-top:10vh">
                 <el-form-item label="专业">
-                  <el-input v-model="form.major"></el-input>
+                  <el-input v-model="worker.major"></el-input>
                 </el-form-item>
                 <el-form-item label="工作领域">
                   <el-input v-model="worker.workArea"></el-input>
                 </el-form-item>
-                <el-form-item label="工作地点">
-                  <el-input v-model="form.workPlace"></el-input>
+                <el-form-item label="学历水平">
+                  <el-input v-model="worker.education"></el-input>
                 </el-form-item>
-            
-                <el-form-item label="上网时长">
-                  <el-select v-model="form.worktime" placeholder="">
-                    <el-option label="1h" value="1h"></el-option>
-                    <el-option label="2h" value="2h"></el-option>
-                    <el-option label="3h" value="3h"></el-option>
-                    <el-option label="4h" value="4h"></el-option>
-                    <el-option label="5h" value="5h"></el-option>
-                    <el-option label="6h" value="6h"></el-option>
-                    <el-option label="7h" value="7h"></el-option>
-                    <el-option label="8h" value="8h"></el-option>
-                    <el-option label="9h" value="9h"></el-option>
-                    <el-option label="10h" value="10h"></el-option>
-                    <el-option label="11h" value="11h"></el-option>
-                    <el-option label="12h" value="12h"></el-option>
-                    <el-option label="13h" value="13h"></el-option>
-                    <el-option label="14h" value="14h"></el-option>
-                    <el-option label="15h" value="15h"></el-option>
-                    <el-option label="16h" value="16h"></el-option>
-                    <el-option label="17h" value="17h"></el-option>
-                    <el-option label="18h" value="18h"></el-option>
-                    <el-option label="19h" value="19h"></el-option>
-                    <el-option label="20h" value="20h"></el-option>
-                    <el-option label="21h" value="21h"></el-option>
-                    <el-option label="22h" value="22h"></el-option>
-                    <el-option label="23h" value="23h"></el-option>
-                    <el-option label="24h" value="24h"></el-option>
-                  </el-select>
+                <el-form-item label="等级">
+                  <span style="margin-left:2vh">{{worker.level}}</span>
                 </el-form-item>
                 <el-form-item>
                   <el-button type="primary">保存</el-button>
@@ -133,36 +97,15 @@
           </el-collapse>
           <el-collapse accordion v-if="information_page_menu==1.2">
             <template>
-
-              <el-card class="box-card">
-              <div slot="header" class="clearfix">
-              <span>余额信息</span>
-              </div>
-
-              </el-card>
+              <section>余额
+              </section>
             </template>
           </el-collapse>
           <el-collapse accordion v-if="information_page_menu==1.3">
             <template>
-                  <el-table
-                  :data="tableData"
-                   style="width: 100%">
-                   <el-table-column
-                   prop="date"
-                   label="日期"
-                    width="180">
-                </el-table-column>
-                 <el-table-column
-                  prop="money"
-                   label="提现金额"
-                   width="180">
-                   </el-table-column>
-                    <el-table-column
-                   prop="method"
-                     label=提现方式>
-                  </el-table-column>
-                   </el-table>
-               </template>
+              <section>提现信息
+              </section>
+            </template>
           </el-collapse>
         </el-col>
       </el-row>
@@ -175,10 +118,11 @@
   import axios from 'axios'
   import WorkerHomepageTopbar from '@/components/WorkerNavi/WorkerHomepageTopbar.vue'
 
+
   export default {
-     components:{
-        WorkerHomepageTopbar,
-      },
+    components:{
+      WorkerHomepageTopbar,
+    },
     methods: {
       BasicInformation(){
         this.information_page_menu=0.0;
@@ -245,24 +189,7 @@
         input_advice: '',
         information: [],
         activeNames: [],
-        form:
-          {
-            name: '',
-            region: '',
-            gender: '',
-            graduation: '',
-            workdomain: '',
-            workPlace: '',
-            worktime: '',
-            internettime: '',
-            major: '',
-            background: '',
-            email: '',
-            password: '',
-            number: '',
-            money:'',
-            method:'',
-          },
+        activeName: '1',
         worker: {
           age: 0,
           balance: 0,
@@ -274,9 +201,10 @@
           withdrawnMethod: '',
           workArea: '',
           workerId:'',
+          major:'',
+          level:'',
         },
       }
-
     },
 
     created()
@@ -299,25 +227,6 @@
   }
 </script>
 
-<style>
-.text {
-    font-size: 14px;
-  }
+<style scoped>
 
-  .item {
-    margin-bottom: 18px;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
-
-  .box-card {
-    width: 480px;
-  }
 </style>
