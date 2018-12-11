c3671a3fab70edb46ac0081f8d00847fae948e22
HumbabaZ <972230255@qq.com>
2018-12-11 20:25:15 +0800

    Update RequesterHomepageDetail.vue

--- src/views/requester/RequesterHomepageDetail.vue
+++ src/views/requester/RequesterHomepageDetail.vue
@@ -2,35 +2,41 @@
 <div>
     <RequesterHomepageTopbar/>
     <el-row>
-        <el-col :span="4">
+        <el-col :span="4">          
             <RequesterHomepageSidebar/>
         </el-col>
-        <el-col :span="20" style="background-color:#F7F6F6">
+        <el-col :span="20" style="background-color:#F7F6F6">         
                 <div class="basic_info">
                     <div class="basic_info_title">
                         <span>详细信息</span>
                     </div>
                     <div class="box">
-                        <el-form label-position="left" label-width="110px" :model="user" >
+                        <el-form label-position="left" label-width="110px" :model="user" >                                            
                         <el-form-item label="公司/机构名称">
-                        <el-input v-model="requester.institutionName"></el-input>
+                        <el-input v-model="inst"></el-input>
                         </el-form-item>
                         <el-form-item label="地址">
-                            <el-input v-model="requester.address"></el-input>
+                            <el-input v-model="address"></el-input>
                         </el-form-item>
                         <el-form-item label="研究领域">
-                        <el-input v-model="requester.research_field"></el-input>
+                        <el-input v-model="research_field"></el-input>
+                        </el-form-item>
+                        <el-form-item label="研究用途">
+                        <el-input v-model="usage"></el-input>
                         </el-form-item>
                         <el-form-item>
                             <el-button  type="primary" @click="modify" class="login_button">保存信息</el-button>
                         </el-form-item>
+                        
+                        
+                    </el-form-item>
                 </el-form>
                     </div>
                 </div>
-
+            
         </el-col>
     </el-row>
-
+       
 </div>
 </template>
 
@@ -38,48 +44,67 @@
 <script>
 import RequesterHomepageTopbar from '@/components/RequesterNavi/RequesterHomepageTopbar.vue'
 import RequesterHomepageSidebar from '@/components/RequesterNavi/RequesterHomepageSidebar.vue'
-import * as axios from 'axios'
     export default {
-      components:{
-        RequesterHomepageTopbar,
-        RequesterHomepageSidebar
-      },
-      methods: {
-      },
-      data(){
-        return{
-          requester:{
-            requesterId: 0,
-            username: "",
-            name: "",
-            teleNumber: "",
-            eMail: "",
-            research_field:"",
-            institutionName:'',
-            address: "",
-            payMethod: "",
-            gender:"",
-            age: 30,
-            balance:0,
-            email:'',
-          }
-        }
-      },
-      created(){
-        let that = this;
-        axios({
-          method:	'get',
-          url: '/api/requester/find-myself',
-        })
-          .then(function (response) {
-            console.log(response);
-            that.requester = response.data.requester;
-            that.requester.institutionName = '同济大学';
-          })
-          .catch(function (error) {
-            alert(error);
-          });
-      }
+        components:{
+            RequesterHomepageTopbar,
+            RequesterHomepageSidebar
+        },
+        created:function(){
+            this.getDetails();
+        },
+        methods: {
+            getDetails(){
+                axios(
+                    {
+                        url:'http://localhost:8080/requester/find-by-username',
+                        method:'POST',
+                        params:{
+                            username:this.$route.query.name
+                        }
+                    }
+                ).then((response)=>{
+                    if (response.data.status == 200) {
+                    }
+                    else
+                        throw response;
+                    if (response.data.status == "200") {
+                        this.inst=response.institutionName;
+                        this.address=response.area;
+                        this.research_field=response.research_field;//还没写
+                        this.usage=response.usage;//没写
+                    }
+                }).catch(function (error) {
+                    if (error.status == 500) {
+                    swal("Error", "服务器错误！", "error");
+                    }
+                });     
+            },
+            modify(){
+                axios.post('http://localhost:8080/requester/find-by-username',{
+                    "institutionName":this.inst,
+                    "area":this.address,
+                    "research_field":this.research_field,
+                    "usage":this.usage
+                }).then(function(response){
+                    if(response.status == 200) {
+                    this.$message({
+                    message: '修改成功',
+                    type: 'success'
+                    });
+                    }
+                }).catch(function(error){
+                   
+                    });
+            }
+        },
+        data(){
+           return{
+                inst:inst,
+                address:address,
+                research_field:research_field,
+                usage:usage
+           }
+        },
     }
 </script>
 
